446e3794a60209ce563fde0f9893235806e952fc
yxh <yxh1103@qq.com>
2018-10-26 22:47:17 +0800

    LinearRecycleView

--- app/src/main/java/com/example/administrator/myapplication/recycleview/LineaRecycleViewActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/recycleview/LineaRecycleViewActivity.java
@@ -1,8 +1,13 @@
 package com.example.administrator.myapplication.recycleview;
 
+import android.graphics.Rect;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.administrator.myapplication.R;
 
@@ -14,5 +19,22 @@ public class LineaRecycleViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linea_recycle_view);
         mRvMain = findViewById(R.id.rv_main);
+        mRvMain.setLayoutManager(new LinearLayoutManager(LineaRecycleViewActivity.this));
+        mRvMain.addItemDecoration(new MyDecoration());
+        mRvMain.setAdapter(new LinearAdapter(LineaRecycleViewActivity.this, new LinearAdapter.OnItemClickListener() {
+            @Override
+            public void onclick(int pos) {
+                Toast.makeText(LineaRecycleViewActivity.this,"click postion="+pos,Toast.LENGTH_SHORT).show();
+            }
+        }));
+
     }
+
+    class MyDecoration extends RecyclerView.ItemDecoration{
+        public void getItemOffsets(@NonNull Rect outRect, @NonNull View view, @NonNull RecyclerView parent, @NonNull RecyclerView.State state) {
+            super.getItemOffsets(outRect,view,parent,state);
+            outRect.set(0,0,0,getResources().getDimensionPixelOffset(R.dimen.dividerHeight));
+        }
+    }
+
 }
--- app/src/main/java/com/example/administrator/myapplication/recycleview/LinearAdapter.java
+++ app/src/main/java/com/example/administrator/myapplication/recycleview/LinearAdapter.java
@@ -1,31 +1,57 @@
 package com.example.administrator.myapplication.recycleview;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.administrator.myapplication.R;
+
 
 public class LinearAdapter extends RecyclerView.Adapter<LinearAdapter.LinearViewHolder> {
+    private Context context;
+    private OnItemClickListener mItemClickListener;
+    public LinearAdapter(Context context,OnItemClickListener listener){
+        this.context=context;
+        mItemClickListener = listener;
+    }
+
     @NonNull
     @Override
-    public LinearAdapter.LinearViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public LinearAdapter.LinearViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new LinearViewHolder(LayoutInflater.from(this.context)
+                .inflate(R.layout.layout_linear_item,parent,false));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull LinearAdapter.LinearViewHolder viewHolder, int i) {
-
+    public void onBindViewHolder(@NonNull LinearAdapter.LinearViewHolder viewHolder, int position) {
+        viewHolder.textView.setText("hello word");
+        viewHolder.textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mItemClickListener.onclick(position);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 30;
     }
 
     class LinearViewHolder extends RecyclerView.ViewHolder{
-
+        private TextView textView;
         public LinearViewHolder(@NonNull View itemView) {
             super(itemView);
+            textView = itemView.findViewById(R.id.rv_tv_title);
         }
+    }
+
+    public interface OnItemClickListener{
+        void onclick(int pos);
     }
 }
